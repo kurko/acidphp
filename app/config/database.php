a409504972de5523d5d99f6a1f81dfdcd5e04112
kurko <chavedomundo@gmail.com>
2009-07-21 16:23:53 -0300

    Inicialização de Models

---
+++ app/config/database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Configura��es de acesso ao Banco de Dados
+ *
+ */
+/**
+ * Conexão local de acesso ao DB com conte�dos
+ */
+    $dbConn = array(
+        'server' => 'localhost',
+        'database' => 'aust',
+        'username' => 'root',
+        'password' => '',
+        'encoding' => 'utf8',
+    );
+
+                /*
+                    $central['db']['servidor'] = '192.168.2.107';
+                    $central['db']['db'] = 'acgrupo';
+                    $central['db']['usuario'] = 'root';
+                    $central['db']['senha'] = '';
+                 *
+                 */
+
+?>
