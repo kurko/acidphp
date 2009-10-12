8f075725e188fd20a3dd7a1f1f7b58f9dcfcc046
kurko <chavedomundo@gmail.com>
2009-10-11 21:09:31 -0300

    Generate Controller pronto. O View relativo ao controller também é criado automaticamente. [aao]

---
+++ core/support/view/default_view.php
@@ -0,0 +1,27 @@
+<?php
+/* *
+ * VIEW NAME
+ *
+ * Use this line to comment your View
+ *
+ * @package View
+ * @name Your View Name
+ * @author Your name here <youremail@here.com>
+ * @since v0.x.x xx/xx/xxxx
+ */
+
+/*
+ * Some divs to help you start coding faster
+ */
+/*
+
+<div id="">
+    <div class="">
+    </div>
+
+    <div class="">
+    </div>
+</div>
+
+ */
+?>
