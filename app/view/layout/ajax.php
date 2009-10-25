06b85b287b50be1dca150608eee78ed2f558de22
kurko <chavedomundo@gmail.com>
2009-10-25 15:55:46 -0200

    Comentários em view/layout/ajax.php

--- app/view/layout/ajax.php
+++ app/view/layout/ajax.php
@@ -9,4 +9,12 @@
  * @author Alexandre de Oliveira <chavedomundo@gmail.com>
  * @since v0.0.6 16/07/2009
  */
+/*
+ * Este layout é vazio, ideal para quando se retorna dados de requisições
+ * Ajax.
+ *
+ * Use setAjax() no controller para automaticamente desativar o debug e
+ * usar este layout para requisições Ajax.
+ */
+echo $content_for_layout;
 ?>
